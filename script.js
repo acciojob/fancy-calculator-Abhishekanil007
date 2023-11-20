7dc233e43fd0d46680e2410d5cb2af306eb95521
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-20 15:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let firstValue = prompt("Enter first value ");
+let secondValue = prompt("Enter second value");
+
+let firstMultiple = parseInt(firstValue * 2); 
+
+let result = firstMultiple + parseInt(secondValue);
+
+alert("The final result is: "+ result);
